923c0ae5febb0bd1caef4ff89acc68743f8502c2
arlan-6 <119741699+arlan-6@users.noreply.github.com>
2023-09-21 23:20:37 +0600

    start taxs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Calculator from "./components/calculator/calculator";
+import Taxs from "./components/taxs/taxs";
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Taxs />
     </div>
   );
 }
--- src/components/taxs/taxs.js
+++ src/components/taxs/taxs.js
@@ -1,3 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const 
+const Taxs = () => {
+  <>
+    <div className="container">
+      <input type="text" name="name" />
+      <input type="number" />
+      <button>Create</button>
+    </div>
+  </>;
+};
+export default Taxs;
